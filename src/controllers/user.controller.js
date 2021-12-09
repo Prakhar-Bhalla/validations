ea81132c1a888094147d4022f136f62c41d68a62
Prakhar-Bhalla <prakharbhalla2016@gmail.com>
2021-12-10 01:47:25 +0530

    validation final

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,26 +6,20 @@ const User = require("../models/user.model");
 
 const { body, validationResult } = require('express-validator');
 
-router.post("/", body("first_name").notEmpty().withMessage("First name can't be empty"), body("last_name").notEmpty().withMessage("Last name can't be empty"), body("email").notEmpty().withMessage("email can't be empty").isEmail().withMessage("Invalid Email"), body("pincode").notEmpty().withMessage("Pincode can't be empty").custom((value) => {
-    if(value == undefined)
-    return;
+router.post("/", body("first_name").notEmpty().withMessage("First name can't be empty"), body("last_name").notEmpty().withMessage("Last name can't be empty"), body("email").notEmpty().withMessage("email can't be empty").isEmail().withMessage("Invalid Email"), body("pincode").custom((value) => {
     const pin = value.toString();
     if(pin.length != 6) 
     {
         throw new Error("Pincode can only contain 6 numerical digits");
     }
     return true;
-}), body("age").notEmpty().withMessage("Age can't be empty").custom((value) => {
-    if(value == undefined)
-    return;
+}), body("age").custom((value) => {
     if(value < 1 || value > 100)
     {
         throw new Error("Members between age 1 to 100 only allowed to register");
     }
     return true;
-}), body("gender").notEmpty().withMessage("Gender can't be empty").custom((value) => {
-    if(value == undefined)
-    return;
+}), body("gender").custom((value) => {
     let genderOpt = ["Male", "Female", "Others"];
     if(!genderOpt.includes(value))
     {
